efbdf1e8ebc0b7c557a121c659b5140c7e693ea3
SanderBackx1 <sander.backx123@gmail.com>
2021-04-02 12:00:35 +0200

    draw rect function

--- grab_frames.py
+++ grab_frames.py
@@ -19,6 +19,48 @@ frames = 20
 c_hand = list()
 rectangles = list()
 sections = list()
+
+
+
+
+
+
+def draw_rect(rectangle, frame):
+    x,y,w,h = rectangle[0]
+    _,digit,color = rectangle
+    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+
+    font  = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale  = 0.5
+
+
+    fontColor = (0,0,255)
+    
+    lineType= 2
+    x,y,w,h = rectangle[0]
+    bottomLeftCornerOfText = (x + int(w/2)-5,y + int(h/3)+int(h/3))
+    if rectangle[2]=='blue':
+        fontColor = (255,0,0)
+    elif rectangle[2] =='black':
+        fontColor = (0,0,0)
+    elif rectangle[2] =='red':
+        fontColor=(0,0,255)
+    elif rectangle[2] == 'yellow':
+        fontColor = (90,145,250)
+    cv2.putText(frame,f'{rectangle[1]}', 
+        bottomLeftCornerOfText, 
+        font, 
+        fontScale,
+        fontColor,
+        lineType)
+
+
+    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
+
+
+
+
+
 while True:
     hand = list()
     game_board = list()
@@ -72,28 +114,28 @@ while True:
             #     print((msg))
 
         predict = False
+        for rectangle in rectangles:
+            x,y,w,h = rectangle[0]
+            _,digit,color = rectangle
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+
+            if y >350:
+                hand.append([digit, color])
+            else:
+
+                for section in sections:
+                    sx,sy,sw,sh = section[0]
+                    margin = 5
+
+                    if x>sx-margin and x+w < sx+sw+margin and y>sy-margin and y+h<sy+sh:
+                        section[1].append(rectangle)
+            font  = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale  = 0.5
         c_hand = divide_colors(hand)
         print('-------------------------------------------------------------------------------------------')
         # print(c_hand['blue'])
         print('black',check_if_move_possible(c_hand['black']), ' red',check_if_move_possible(c_hand['red']))
         print('yellow',check_if_move_possible(c_hand['yellow']), ' blue',check_if_move_possible(c_hand['blue']))
-        for rectangle in rectangles:
-                x,y,w,h = rectangle[0]
-                _,digit,color = rectangle
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
-
-                if y >350:
-                    hand.append([digit, color])
-                else:
-
-                    for section in sections:
-                        sx,sy,sw,sh = section[0]
-                        margin = 5
-
-                        if x>sx-margin and x+w < sx+sw+margin and y>sy-margin and y+h<sy+sh:
-                            section[1].append(rectangle)
-                font  = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale  = 0.5
 
         for section in sections:
             msg = 'section with '
@@ -109,61 +151,13 @@ while True:
     if draw_rects:
         if len(rectangles)>0:
             for rectangle in rectangles:
-                x,y,w,h = rectangle[0]
-                _,digit,color = rectangle
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+                draw_rect(rectangle, frame)
 
-                font  = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale  = 0.5
-
-
-                fontColor = (0,0,255)
-                
-                lineType= 2
-                x,y,w,h = rectangle[0]
-                bottomLeftCornerOfText = (x + int(w/2)-5,y + int(h/3)+int(h/3))
-                if rectangle[2]=='blue':
-                    fontColor = (255,0,0)
-                elif rectangle[2] =='black':
-                    fontColor = (0,0,0)
-                elif rectangle[2] =='red':
-                    fontColor=(0,0,255)
-                elif rectangle[2] == 'yellow':
-                    fontColor = (90,145,250)
-                cv2.putText(frame,f'{rectangle[1]}', 
-                    bottomLeftCornerOfText, 
-                    font, 
-                    fontScale,
-                    fontColor,
-                    lineType)
-
-    
-                cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
-
-
-
-
-
-
-
-
-    # print('--------------------')
-
-
-
-
-
-    # for section in game_board:
-    #     msg = 'section with '
-    #     for block in section:
-    #         msg+= f' {block[1]} {block[2]} '
-    
-        # print(msg)
-    # print(game_board)
     
 
     cv2.imshow('Game', frame)
 
             
 cv2.destroyAllWindows()
+
 
