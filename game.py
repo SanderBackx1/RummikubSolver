57bfb8fb8d4503206f1cccb0879f968375bedfea
SanderBackx1 <sander.backx123@gmail.com>
2021-05-28 11:46:22 +0200

    add excess blocks move

--- game.py
+++ game.py
@@ -222,34 +222,50 @@ from mcts import MCTS, Node
 _RKB = namedtuple("RummikubBoard", "tup instructions hand bag turn_moves inhand turn winner terminal moves_zero")
 
 class RummikubBoard(_RKB, Node):
-        
-    def move_section_excess_to_hand(board, all_sections,hand):
-        for section in all_sections:
-            if len(section)>3:
-                digits, colors, section_type  = board.check_section_type(section)
+   
+    def move_section_excess(board, all_sections):
+        excess = list()
+        for i, section in enumerate(all_sections):
+            cur_section = sorted(section, key=lambda a: a.digit)
+            if len(cur_section)>3:
+                digits, colors, section_type  = board.check_section_type(cur_section)
+                lst = [(x.digit, x.color) for x in board.hand]
                 if section_type == 's':
-                    for block in section:
-                        if block.digit not in [x.digit for x in hand]:
-                            hand.append(block)
-                            board.instructions += f"Using {block.digit} {block.color} from {[f'{x.digit} {x.color}' for x in section]}"
+                    s_lst = [x for x in cur_section if x.digit == 0]
+                    if len(s_lst)>0:
+                        excess.append(s_lst[0])
+                    else:
+                        block = choice(cur_section)
+                        if (block.digit, block.color) not in lst:
+                                excess.append( block) 
+                            # board.instructions += f"Using {block.digit} {block.color} from {[f'{x.digit} {x.color}' for x in section]}"
                 else:
-                    
-                    if section[-1].digit not in [x.digit for x in hand]:
-                        hand.append(section[-1])
-                        board.instructions += f"Using {block.digit} {block.color} from {[f'{x.digit} {x.color}' for x in section]}"
-                    elif section[0].digit not in [x.digit for x in hand]:
-                        hand.append(section[0])
-                        board.instructions += f"Using {block.digit} {block.color} from {[f'{x.digit} {x.color}' for x in section]}"
+                    if (cur_section[-1].digit, cur_section[-1].color) not in lst:
+                        excess.append(cur_section[-1])
+                        # board.instructions += f"Using {block.digit} {block.color} from {[f'{x.digit} {x.color}' for x in cur_section]}"
+                    elif (cur_section[0].digit, cur_section[0].color) not in lst:
+                        excess.append(cur_section[0])
+                        # board.instructions += f"Using {block.digit} {block.color} from {[f'{x.digit} {x.color}' for x in section]}"
+        return excess
 
     def all_possible_moves(board,all_sections,hand):
         moves = list()
 
+        excess = board.move_section_excess(board.tup)
         cur_hand = list(board.hand)
+        cur_hand.extend(excess)
+        # print('excess -------------')
+        # for x in excess:
+        #     print(f'{x.digit} {x.color}')
+        # print('-----------------')
         cur_hand = sorted(cur_hand, key=lambda a: a.digit)
+        # print('Hand -------------')
+        # for h in cur_hand:
+        #     print(f'{h.digit} {h.color}')
+        # print('-------------------')
         cur_move_counting = list()
         cur_move_same = list()
-
-        board.move_section_excess_to_hand(all_sections,hand)
+  
         for i in range(len(cur_hand)-1):
             cur_move_counting.clear()
             cur_move_same.clear()
@@ -278,12 +294,12 @@ class RummikubBoard(_RKB, Node):
             for blockidx,block in enumerate(hand):
                 if board.check_if_valid(section, block):
                     
-
                     moves.append((block,section,blockidx,sectionidx))
         return tuple(moves)
 
-    def check_hand_for_moves(hand):
-        cur_hand = self.hand
+
+    def check_hand_for_moves(board,hand):
+        cur_hand = board.hand
         cur_hand.sort()
         cur_move = list()
         possible_moves = list()
@@ -305,6 +321,7 @@ class RummikubBoard(_RKB, Node):
                     possible_moves.append([x for x in cur_move])
                 cur_move.clear()
         return tuple(possible_moves)
+
 
     def check_section_type(self,section):
         digits = [block.digit for block in section]
@@ -352,19 +369,20 @@ class RummikubBoard(_RKB, Node):
             else:
                 return board.make_move(choice(apm))
                     
-    
+        
     def reward(board):
         if not board.terminal:
             raise RuntimeError(f"reward called on nonterminal board {board}")
-        if board.winner is board.turn:
-            #It's your turn and you've already won
-            raise RuntimeError(f"reward called on unreachable board {board}")
+        # if board.winner is board.turn:
+        #     #It's your turn and you've already won
+        #     raise RuntimeError(f"reward called on unreachable board {board}")
         
         if board.turn is (not board.winner):
             return 0
         if board.winner is None:
-            return board.turn_moves + 0.5 #should never happen in rummikub
-
+            return board.turn_moves + 0.5 
+        if board.winner is True:
+            return 100
         raise RuntimeError(f"Board has unknown winner type {board.winner}")
 
     def is_terminal(board):
@@ -382,12 +400,12 @@ class RummikubBoard(_RKB, Node):
         if section_type == 's':
             # check if same digit check if different color check if joker
             joker_color = ''
-            for block in section:
-                if block.digit == 0:
-                    joker_color = block.color
+            for b in section:
+                if b.digit == 0:
+                    joker_color = b.color
                 
-            
-            if (digits[0]== block.digit and (block.color not in colors or block.color == joker_color)) or block.digit == 0:
+
+            if (digits[1]== block.digit and (block.color not in colors or block.color == joker_color)) or block.digit == 0:
                 valid=True
         else:
             # check if same color check if counting 
@@ -423,7 +441,6 @@ class RummikubBoard(_RKB, Node):
 
     def make_move(board, move):
         block, section, blockidx, sectionidx = move
-        
         hand = list(board.hand)
         bag = list(board.bag)
         turn = board.turn
@@ -432,6 +449,7 @@ class RummikubBoard(_RKB, Node):
         inhand= board.inhand
         moves_zero = board.moves_zero
         tup = board.tup
+        
         if block == -1:
             if turn_moves == 0:
                 if board.turn: 
@@ -445,6 +463,7 @@ class RummikubBoard(_RKB, Node):
             turn_moves=0
         
         else:
+
             tup = list(board.tup)
             tup = [sorted(x, key=lambda y: y.digit) for x in tup ]
             tup = tuple(tup)
@@ -459,7 +478,7 @@ class RummikubBoard(_RKB, Node):
                         if len(add)>0:
                             hand.remove(blck)    
                 else:
-                    instructions+=f"\nMove block {block.digit} {block.color} to section {[f'{x.digit} {x.color}' for x in tup[sectionidx] if sectionidx  -1]}"
+                    instructions+=f"\nMove block {block.digit} {block.color} to section {[f'{x.digit} {x.color}' for x in tup[sectionidx] if sectionidx > -1]}"
                     add=[x for x in hand if x.digit == block.digit and x.color == block.color]
                     hand.remove(add[0])
             else:
@@ -475,14 +494,15 @@ class RummikubBoard(_RKB, Node):
                     inhand-=1
             
 
-
-        if board.turn and hand==0:
+        if board.turn and len(hand)<=0:
             winner = True
-        elif not board.turn and inhand==0:
+        elif not board.turn and inhand<=0:
             winner = True
         elif moves_zero > 5:
             winner = None
             moves_zero = 0
+        elif turn_moves > 100:
+            winner = None
         else:
             winner = False        
 
